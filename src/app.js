d07163229613cf3a0b0989dd319b19daa5f8aac3
JuanmaGF <137336081+JuanmaGF@users.noreply.github.com>
2023-11-29 20:25:08 +0000

    css

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let numeroCarta = [
+  "A",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let paloCarta = ["♦", "♥", "♠", "♣"];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numRandom = Math.floor(Math.random() * 14);
+  let paloRandom = Math.floor(Math.random() * 4);
 };
