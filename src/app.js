37e26dc5a1dd07539f8da86f698bec4d9830b2f0
JuanmaGF <137336081+JuanmaGF@users.noreply.github.com>
2023-11-30 12:11:55 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -1,26 +1,32 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
-let numeroCarta = [
-  "A",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K"
-];
-let paloCarta = ["♦", "♥", "♠", "♣"];
 
 window.onload = function() {
   //write your code here
-  let numRandom = Math.floor(Math.random() * 14);
-  let paloRandom = Math.floor(Math.random() * 4);
+  let carta = document.querySelector(".card");
+  let boton = document.querySelector("#boton");
+  boton.addEventListener("click", function() {
+    let numeroCarta = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let paloCarta = ["diamonds", "hearts", "clubs", "spades"];
+    let numRandom = numeroCarta[Math.floor(Math.random() * 13)];
+    let paloRandom = paloCarta[Math.floor(Math.random() * 4)];
+    carta.classList.remove("spades", "diamonds", "hearts", "clubs");
+    carta.classList.add(`${paloRandom}`);
+    carta.innerHTML = numRandom;
+  });
 };
